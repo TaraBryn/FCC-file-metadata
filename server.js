22acf258dc38eda653e8f3f6a62081cba3e93ecd
Glitch (tarabryn-file-metadata-microservice) <none>
2019-10-28 03:19:30 +0000

    👰👠 Checkpoint ./package.json:7351991/1999 ./server.js:7351991/229

--- server.js
+++ server.js
@@ -4,8 +4,14 @@ var express = require('express');
 var cors = require('cors');
 
 // require and use "multer"...
+var multer = require('multer');
+var upload = multer({dest: 'updloads/'});
 
 var app = express();
+
+app.post('/api/fileanalyse', upload.single('file'), function(req, res){
+  res.json({file: req.file});
+});
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
